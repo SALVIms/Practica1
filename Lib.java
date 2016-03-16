4f94aa0ee02c480597bfa60bbf9dc020cba1f385
luisfrap <luisfraipas@gmail.com>
2016-03-16 18:27:40 +0100

    Modificaciones alumno 2

--- Lib.java
+++ Lib.java
@@ -11,6 +11,7 @@ public class Lib {
 
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	System.out.println("modificado por Alumno2);	// Alumno 2
     }
     public void metodo3 () {
 	System . out . println (" metodo 3");
@@ -18,5 +19,7 @@ public class Lib {
 
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	//Alumno 2
+	System.out.println("Alumno 2 inserta esta sentencia);
     }
 }
